4da2f2c0a60f91903a203c021dcea045f4fb55dd
Maciej Koch <maciej.k00h@gmail.com>
2020-05-07 20:25:37 +0200

    Updates

---
+++ precache-manifest.0aa4a913e290d454b74ee3333cf68970.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4363c4c6c1e179cc5a4aa1d40d23e630",
+    "url": "/movie-search-app/index.html"
+  },
+  {
+    "revision": "15707069f455d33f92ad",
+    "url": "/movie-search-app/static/css/main.ba4ecdeb.chunk.css"
+  },
+  {
+    "revision": "1f28c80254174c98e424",
+    "url": "/movie-search-app/static/js/2.a5a00b2c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-search-app/static/js/2.a5a00b2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15707069f455d33f92ad",
+    "url": "/movie-search-app/static/js/main.e85e8a9b.chunk.js"
+  },
+  {
+    "revision": "31e0431b9a9fd60e8693",
+    "url": "/movie-search-app/static/js/runtime-main.2f076575.js"
+  }
+]);
